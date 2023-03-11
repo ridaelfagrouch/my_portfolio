bebaadf83e007c2599cef7c0409c5fd96a749030
G44r4 <elfagrouchrida21@gmail.com>
2023-03-11 23:39:12 +0100

    init my_portfolio   - add navbar and make it responsive 	- add navbar and make it resposive

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Blogs = () => {
+    return (
+        <div>Blogs</div>
+    );
+}
+ 
+export default Blogs;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
+
+const Navbar = () => {
+  const [active, SetActive] = useState("");
+  const [toggle, SetToggle] = useState(false);
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex justify-between items-center fixed top-0 left-0 z-50 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            SetActive("")
+            window.scrollTo(0, 0)
+          }
+          }
+        >
+          <img src={logo} alt="logo" className="w-100 h-10 object-contain" />
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => SetActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => SetToggle(!toggle)}
+          />
+          {/* mobile mode---------------------------------------------------------------------------- */}
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    SetToggle(!toggle);
+                    SetActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* ---------------------------------------------------------------------------- */}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
